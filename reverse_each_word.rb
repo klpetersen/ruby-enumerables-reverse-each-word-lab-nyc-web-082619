78de3f945b20c134415a25445a87d12925c18155
Kayla Petersen <petersen1994@gmail.com>
2019-08-19 21:54:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,7 @@
 def reverse_each_word(string)
   each_array = []
   new_array = string.split(" ")
-  new_array.each {|string| return_array << string.reverse} 
+  new_array.collect {|string| return_array << string.reverse} 
   return_array.join(" ")
-  
-  collectArray = []
-  new_collect_array = string.split(" ")
-  new_collect_array.collect {|string| collect_return << string.reverse} 
-  collect_return.join(" ")
 end
 
